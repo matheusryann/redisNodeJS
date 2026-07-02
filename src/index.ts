915bb74ee507f11211c7fecbb53514d0cea04101
matheusryann <ryanncarreiro09@gmail.com>
2026-07-02 14:08:41 -0300

    Refactor email job handling and improve server startup logging

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,28 @@ import "dotenv/config";
 import express from "express";
 
 
+
+const PORT = process.env.PORT ?? 3000;
 const app = express();
 
 app.use(express.json());
+
+
+app.post("/jobs/email", async (req, res) => {
+    const {to, subject, body} = req.body;
+    console.log(`Sending email to ${to} with subject "${subject}" and body "${body}"`);
+
+    return res.status(202).json({
+        message: "Job added to the queue",
+        jobId: 123
+    });
+});
+
+app.get("/jobs/:jobId", async (req, res) => {
+    return res.json({ok: true})
+})
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on  http://localhost:${PORT}`);
+})
